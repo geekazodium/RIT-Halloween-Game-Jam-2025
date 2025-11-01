0bb3f7670cbc0c12a87b3daa9ba9adfaf5b88f60
meeeeeepking <joaquin.parada@outlook.com>
2025-11-01 15:02:38 -0400

    things work yay

--- room.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@export var containers : Array
-
-@export var camera_z_relative : float
-@export var camera_y_relative : float
-@export var camera_fov : float
-@export var camera_x_rotation : float
