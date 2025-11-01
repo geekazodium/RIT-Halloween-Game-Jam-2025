28bf13a4d3f7eb77c55b53607c06cf4755896082
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-10-31 22:26:58 -0400

    feature: roommate suspicion

--- roommate/player_detection_radius.gd
+++ roommate/player_detection_radius.gd
@@ -4,9 +4,14 @@ class_name PlayerDetectionRadius
 @export_range(0, 180, 0.001, "radians_as_degrees") var vision_angle_range: float = PI/4;
 @export var direction_src: UMCharacterBody3D;
 
-func _physics_process(_delta: float) -> void:
+@export var suspicion_decay_per_second: float = .1;
+var suspicion: float = 0;
+
+func _physics_process(delta: float) -> void:
 	var facing: Vector2 = direction_src.last_nonzero_input_dir.normalized();
 	var dot_prod_tolerance: float = cos(self.vision_angle_range);
+	
+	var suspicious: bool = false;
 	
 	for body:PhysicsBody3D in self.get_overlapping_bodies():
 		## uncomment when player type exists
@@ -16,5 +21,10 @@ func _physics_process(_delta: float) -> void:
 		var dot: float = offset_x_z.dot(facing);
 		if dot < dot_prod_tolerance:
 			continue;
-		print("shape detected");
-	pass;
+		self.suspicion += self._get_suspicion_per_second(body as CharacterBody3D) * delta;
+		suspicious = true;
+	if !suspicious:
+		self.suspicion = max(self.suspicion - self.suspicion_decay_per_second * delta,0);
+
+func _get_suspicion_per_second(player: CharacterBody3D) -> float:
+	return 1;
--- roommate/test_cursor.gd
+++ roommate/test_cursor.gd
@@ -10,12 +10,11 @@ var plane_norm: Vector3:
 var plane_point: Vector3:
 	get: 
 		return plane_norm * plane.z;
-#@export var camera: Camera3D;
 
 func _physics_process(_delta: float) -> void:
 	var camera: Camera3D = self.get_viewport().get_camera_3d();
 	var screen_pos: Vector2 = camera.get_viewport().get_mouse_position();
-	#// calculate ray direction using 2 points projected at different depths, this will account for how the camera projection is set up
+	## calculate ray direction using 2 points projected at different depths, this will account for how the camera projection is set up
 	var ray_start: Vector3 = camera.project_position(screen_pos, 0.);
 	var ray_dir: Vector3 = camera.project_position(screen_pos, 10.) - ray_start;
 		
