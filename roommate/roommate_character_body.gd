745c83d0d1b5a69a79dca04fd3dff6308bfb971f
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-11-02 00:38:52 -0400

    feature: roommate is added to main scene!

--- roommate/roommate_character_body.gd
+++ roommate/roommate_character_body.gd
@@ -11,6 +11,9 @@ var suspicion: float:
 @export var ideal_speed: float = 5;
 @export var input_force_strength: float = 10;
 
+@export var sprite: AnimatedSprite3D;
+var looking_forward: bool = false;
+
 var held_item: Item;
 
 func _get_ideal_speed() -> float:
@@ -18,3 +21,34 @@ func _get_ideal_speed() -> float:
 
 func _get_input_force_strength() -> float:
 	return self.input_force_strength;
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta);
+	var direction_vec: Vector2 = self.movement_direction.get_input_xz().rotated(self.rotation.y);
+	
+	if sign(direction_vec.y) == 1:
+		looking_forward = true;
+	elif sign(direction_vec.y) == -1:
+		looking_forward = false;
+	
+	if sign(direction_vec.x) == 1:
+		sprite.flip_h = true;
+	elif sign(direction_vec.x) == -1:
+		sprite.flip_h = false;
+	
+	var animation = ""
+	
+	if held_item != null:
+		animation = "holding_"
+	
+	if(direction_vec == Vector2.ZERO):
+		animation += "standing_"
+	else:
+		animation += "walking_"
+	
+	if(looking_forward):
+		animation += "front"
+	else: 
+		animation += "back"
+	
+	sprite.play(animation);
