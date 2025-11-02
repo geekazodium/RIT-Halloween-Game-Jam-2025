b13901fb6b6174605782303dbbeee84fd13cecf1
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-11-02 00:02:41 -0400

    feature: roommate line of sight

--- roommate/player_detection_radius.gd
+++ roommate/player_detection_radius.gd
@@ -6,9 +6,15 @@ class_name PlayerDetectionRadius
 
 @export var suspicion_decay_per_second: float = .1;
 var suspicion: float = 0;
+@export var display_mesh: Node3D;
+
+var line_of_sight: RayCast3D:
+	get:
+		return $SuspicionLineOfSight
 
 func _physics_process(delta: float) -> void:
 	var facing: Vector2 = direction_src.last_nonzero_input_dir.normalized();
+	self.display_mesh.global_rotation.y = -facing.angle();
 	var dot_prod_tolerance: float = cos(self.vision_angle_range);
 	
 	var suspicious: bool = false;
@@ -21,10 +27,32 @@ func _physics_process(delta: float) -> void:
 		var dot: float = offset_x_z.dot(facing);
 		if dot < dot_prod_tolerance:
 			continue;
+		
+		if !self._check_line_of_sight(body):
+			continue;
+		
 		self.suspicion += self._get_suspicion_per_second(body as CharacterBody3D) * delta;
 		suspicious = true;
 	if !suspicious:
 		self.suspicion = max(self.suspicion - self.suspicion_decay_per_second * delta,0);
+
+const SUSPICIOUS_LAYER: int = 6;
+
+func _check_line_of_sight(body: PhysicsBody3D) -> bool:
+	if self.is_colliding_with_suspicious_body(body.global_position - Vector3.UP * .3):
+		return true;
+	
+	return self.is_colliding_with_suspicious_body(body.global_position - Vector3.DOWN * .1);
+
+func is_colliding_with_suspicious_body(global_pos: Vector3) -> bool:
+	self.line_of_sight.target_position = global_pos - self.line_of_sight.global_position;
+	self.line_of_sight.target_position *= 1.1;
+	self.line_of_sight.force_raycast_update();
+	var coll = self.line_of_sight.get_collider();
+	if coll is CollisionObject3D:
+		if coll.get_collision_layer_value(SUSPICIOUS_LAYER):
+			return true;
+	return false;
 
 func _get_suspicion_per_second(player: CharacterBody3D) -> float:
 	return 1;
