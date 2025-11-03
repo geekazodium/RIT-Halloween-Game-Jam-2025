979a3f7e1277547b312aacd7604d62be38f6fdec
geekazodium <59382423+geekazodium@users.noreply.github.com>
2025-11-02 22:58:00 -0500

    fix: win and timer

--- menu/time_until_class_indicator.gd
+++ menu/time_until_class_indicator.gd
@@ -3,5 +3,5 @@ extends Control
 func _process(_delta: float) -> void:
 	var time_left: float = RoommateGlobalRef.get_roommate_time_left();
 	$ProgressBar.value = time_left;
-	$TimeUntilClass.text = "Time Until Class: %sm %ss" % [floori(time_left / 60),floori(time_left) % 60];
+	$TimeUntilClass.text = "Time Until Class: %sm %ss" % [floor(time_left / 60.0) + 1 if time_left < 0 else 0,floori(time_left) % 60];
 	$ProgressBar.max_value = RoommateGlobalRef.get_roommate().seconds_until_late;
